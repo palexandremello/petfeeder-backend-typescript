ef02e4d304b326731591b44ccb8bf5025c03772b
Paulo Alexandre Mello <palexandremello@gmail.com>
2021-10-17 18:05:14 -0300

    feat teste

--- src/controller.ts
+++ src/controller.ts
@@ -3,4 +3,3 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response) => response.json({ message: 'teste' }));
-app.listen(3333, () => console.log('teste carai'));
