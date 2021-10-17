7990891af9c05f7d9ef73a6082d0127f3a23a931
Paulo Alexandre Mello <palexandremello@gmail.com>
2021-10-17 17:45:02 -0300

    feat: add server.ts

--- server.ts
+++ server.ts
@@ -3,5 +3,8 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response) => response.json({ message: 'teste' }));
+app.get('/post', (request, response) => {
+  const { a, b } = request.body;
+});
 
 app.listen(3333, () => console.log('teste carai'));
