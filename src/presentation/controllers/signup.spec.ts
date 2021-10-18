1b9c725d9ec9a0914ebf6995b80d6b78193b3623
Paulo Alexandre Mello <palexandremello@gmail.com>
2021-10-17 22:24:47 -0300

    test: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,19 @@
+import SignUpController from './signup'
+describe('SignUp Controller', () => {
+  test('Should return 400 if no first_name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        last_name: 'Mello',
+        email: 'palexandremello@gmail.com',
+        password: '147258',
+        passwordConfirmation: '147258',
+        birthday: '08/10/1994',
+        sex: 'M',
+      },
+    }
+    const httpReponse = sut.handle(httpRequest)
+
+    expect(httpReponse.statusCode).toBe(400)
+  })
+})
